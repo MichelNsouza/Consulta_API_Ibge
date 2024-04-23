ff0dd3a98724d14f52e76a7057bba9353beb243a
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-04-23 11:21:13 +0000

    correções

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ const app = () => {
     cidades: [],
     
     init() {
-      axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-      .then(resposta => {this.estados = resposta.data})
+      axios.get(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      ).then(
+        resposta => {
+          this.estados = resposta.data
+        }
+      )
     },
     
     buscaCidades(estadoSigla){
-      axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + estadoSigla  + "/municipios")
-      .then(resposta => { this.cidades = resposta.data})
+      axios.get(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + estadoSigla  + "/municipios"
+      ).then(
+        resposta => { 
+          this.cidades = resposta.data
+        }
+      )
     },
-
   }
 }
